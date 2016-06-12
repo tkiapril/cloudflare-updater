495d04bf2269b65e565e607249990c6d6febd201
Seo, Myunggyun <tki@tkism.org>
2016-06-12 21:11:13 +0900

    Script now preserves cloud mode and ttl

--- update_cloudflare.py
+++ update_cloudflare.py
@@ -16,16 +16,25 @@ with (pathlib.Path(__file__).resolve().parent / 'config.yaml').open('r') as r:
 for account in update_list:
     records = {}
     p = pyflare.PyflareClient(account['email'], account['token'])
-    for target in account['targets']: 
-        if target['subdomain'].count('.') < 2:
-            zone = target['subdomain']
-        else:
-            zone = target['subdomain'][target['subdomain'].rfind('.', 0, target['subdomain'].rfind('.'))+1:]
+    for target in account['targets']:
+        zone = target['subdomain']
+        if target['subdomain'].count('.') >= 2:
+            zone = zone[zone.rfind('.', 0, zone.rfind('.'))+1:]
         if zone not in records:
             records[zone] = list(p.rec_load_all(zone))
         for record in records[zone]:
             if record['rec_id'] == target['id']:
                 target_record = record
                 break
-        p.rec_edit(zone, 'A', target['id'], target_record['name'], myip, 1, 0)
-        print('Successfully updated {} to {}'.format(target['subdomain'], myip))
+        p.rec_edit(
+            zone,
+            'A',
+            target['id'],
+            target_record['name'],
+            myip,
+            ttl=int(target_record['ttl']),
+            service_mode=int(target_record['service_mode'])
+        )
+        print(
+            'Successfully updated {} to {}'.format(target['subdomain'], myip)
+        )
