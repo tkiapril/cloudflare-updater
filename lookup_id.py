f483df5004e3997e4e88bb5b04a4b8efb04f8fac
Seo, Myunggyun <tki@tkism.org>
2016-06-17 14:33:30 +0900

    Removed dependency to pyflare and updated API to v4

--- lookup_id.py
+++ lookup_id.py
@@ -1,54 +1,89 @@
 import pathlib
-import socket
 import sys
 
-import pyflare
+import requests
 import yaml
 
 
-try:
-    with (
-        pathlib.Path(__file__).resolve().parent / 'config.yaml'
-    ).open('r') as r:
-        update_list = yaml.load(r)
-except Exception:
-    print('Could not load config file.')
-    sys.exit(1)
+def api(endpoint):
+    return 'https://api.cloudflare.com/client/v4{}'.format(endpoint)
 
-domains = sys.argv[1:]
 
-if len(domains) == 0:
-    print('''
-Please provide domain names to lookup as arguments. You can specify which
-record type to find by writing one after colon after domain name:
-for example, example.com:cname.
-    '''.strip())
-flare = []
-
-for account in update_list:
+def main():
     try:
-        flare.append(pyflare.PyflareClient(account['email'], account['token']))
+        with (
+            pathlib.Path(__file__).resolve().parent / 'config.yaml'
+        ).open('r') as r:
+            update_list = yaml.load(r)
     except Exception:
-        print('Login failed for account: {}'.format(account['email']))
+        print('Could not load config file.')
+        sys.exit(1)
 
-for domain in domains:
-    find_type = None
-    if ':' in domain:
-        find_type = domain[domain.find(':')+1:].upper()
-        domain = domain[:domain.find(':')]
-        if find_type == '':
-            print('Record type should be written after the colon.')
-    if find_type is None or find_type != '':
-        for p in flare:
-            records = p.rec_load_all(domain)
+    zone_names = sys.argv[1:]
+
+    if len(zone_names) == 0:
+        print('''
+Please provide zone names (root domain) to lookup as arguments. You can
+specify which record type to find by writing one after colon after zone name:
+for example, example.com:cname.
+        '''.strip())
+    sessions = []
+
+    for user in update_list:
+        session = requests.Session()
+        session.headers.update(
+            {
+                'X-Auth-Email': user['email'],
+                'X-Auth-Key': user['token'],
+                'DNT': 1,
+            }
+        )
+        if session.get(api('/user')).json()['success']:
+            sessions.append(session)
+        else:
+            print('Login failed for account: {}'.format(user['email']))
+
+    for zone_name in zone_names:
+        find_type = None
+        if ':' in zone_name:
+            find_type = zone_name[zone_name.find(':')+1:].upper()
+            zone_name = zone_name[:zone_name.find(':')]
+            if find_type == '':
+                print('Record type should be written after the colon.')
+                continue
+        for session in sessions:
+            print('\n{}:\n\n'.format(session.headers['X-Auth-Email']))
+            zones = session.get(api('/zones')).json()['result']
+            try:
+                zone_id, = [
+                    zone['id'] for zone in zones if zone['name'] == zone_name
+                ]
+            except ValueError:
+                continue
+            records = session.get(
+                api('/zones/{}/dns_records'.format(zone_id))
+            ).json()['result']
             if find_type is not None:
-                records = [item for item in records if item['type'] == find_type]
+                records = [
+                    item for item in records if item['type'] == find_type
+                ]
             for item in records:
                 print(
-                        '{rec_id}:\t{type}\t{name}\t-> {content}'.format(
-                        rec_id=item['rec_id'],
-                        type=item['type'],
-                        name=item['name'],
-                        content=item['content']
+                        '  # {type}\t{name}\t-> {content}\n'
+                        '  {yaml}  # {name}\n'.format(
+                            type=item['type'],
+                            name=item['name'],
+                            content=item['content'],
+                            yaml=yaml.dump(
+                                [
+                                    {
+                                        'zone_id': item['zone_id'],
+                                        'id': item['id']
+                                    }
+                                ]
+                            ).strip()
+                        )
                     )
-            )
+
+if __name__ == '__main__':
+    main()
--- update_cloudflare.py
+++ update_cloudflare.py
@@ -1,40 +1,75 @@
 import pathlib
 import socket
+from pprint import pprint
 
-import pyflare
+import requests
 from yaml import load
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(('8.8.8.8', 53))
-myip = s.getsockname()[0]
-s.close()
+def api(endpoint):
+    return 'https://api.cloudflare.com/client/v4{}'.format(endpoint)
 
-with (pathlib.Path(__file__).resolve().parent / 'config.yaml').open('r') as r:
-    update_list = load(r)
 
-for account in update_list:
-    records = {}
-    p = pyflare.PyflareClient(account['email'], account['token'])
-    for target in account['targets']:
-        zone = target['subdomain']
-        if target['subdomain'].count('.') >= 2:
-            zone = zone[zone.rfind('.', 0, zone.rfind('.'))+1:]
-        if zone not in records:
-            records[zone] = list(p.rec_load_all(zone))
-        for record in records[zone]:
-            if record['rec_id'] == target['id']:
-                target_record = record
-                break
-        p.rec_edit(
-            zone,
-            'A',
-            target['id'],
-            target_record['name'],
-            myip,
-            ttl=int(target_record['ttl']),
-            service_mode=int(target_record['service_mode'])
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 53))
+    myip = s.getsockname()[0]
+    s.close()
+
+    with (
+        pathlib.Path(__file__).resolve().parent / 'config.yaml'
+    ).open('r') as r:
+        update_list = load(r)
+
+    for user in update_list:
+        session = requests.Session()
+        session.headers.update(
+            {
+                'X-Auth-Email': user['email'],
+                'X-Auth-Key': user['token'],
+                'DNT': 1,
+            }
         )
-        print(
-            'Successfully updated {} to {}'.format(target['subdomain'], myip)
-        )
+        for target in user['targets']:
+            endpoint = api(
+                '/zones/{zone_id}/dns_records/{id}'.format(
+                    zone_id=target['zone_id'],
+                    id=target['id']
+                )
+            )
+            record = session.get(endpoint).json()['result']
+            if record['type'] != 'A':
+                print(
+                    'zone_id {}, id {} ({} {}) is not an A record; '
+                    'not doing anything.'.format(
+                        record['zone_id'],
+                        record['id'],
+                        record['type'],
+                        record['name']
+                    )
+                )
+                continue
+            result = session.put(
+                endpoint,
+                json={
+                    'type': 'A',
+                    'name': record['name'],
+                    'content': myip
+                }
+            ).json()
+            if result['success']:
+                record = result['result']
+                print(
+                    'Successfully updated {} to {}'.format(
+                        record['name'], myip
+                    )
+                )
+            else:
+                print(
+                    'An error occurred while updating {}: '
+                    'Please see below for details.\n'.format(record['name'])
+                )
+                pprint(result)
+
+if __name__ == '__main__':
+    main()
